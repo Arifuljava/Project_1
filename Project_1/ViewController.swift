de8035d06f10475e9bad523dd9ecf2b588a6faa3
Ariful0007 <arifulpub143@gmail.com>
2023-05-04 09:30:01 +0600

    Commit

--- Project_1/ViewController.swift
+++ Project_1/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        FirebaseApp.configure()
+        
     }
 
 
