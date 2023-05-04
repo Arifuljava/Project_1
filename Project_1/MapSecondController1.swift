b9907a3bdbbfa2e3eb117264232d02a630daf6ba
Ariful0007 <arifulpub143@gmail.com>
2023-05-04 12:03:47 +0600

    lo

---
+++ Project_1/MapSecondController1.swift
@@ -0,0 +1,20 @@
+//
+//  MapSecondController1.swift
+//  Project_1
+//
+//  Created by sang on 13/10/1444 AH.
+//
+
+import UIKit
+
+class MapSecondController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- Project_1/ViewController.swift
+++ Project_1/ViewController.swift
@@ -9,19 +9,36 @@ import UIKit
 import FirebaseCore
 import FirebaseAuth
 
-class ViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
+class ViewController: UIViewController {
     
-    @IBOutlet weak var lebel: UIView!
+    
+    @IBOutlet weak var logoimage: UIImageView!
+    
+    
+    @IBOutlet weak var apps_name: UILabel!
+    
+   
+    @IBOutlet weak var viewaa: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         FirebaseApp.configure()
+        let blueOn = UITapGestureRecognizer(target: self, action: #selector(myblueisopen(sender:)))
+        logoimage.isUserInteractionEnabled = true
+        logoimage.addGestureRecognizer(blueOn)
+        //checking blueooth
+        
+        
+}
+    @objc func myblueisopen(sender :UITapGestureRecognizer)
+    {
+        let sec = storyboard?.instantiateViewController(identifier: "mainmap") as! MapSecondController1
+            present(sec,animated: true)
         
     }
-
-    
+   
     
 }
 
